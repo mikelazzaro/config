30ca668525685e0a5ea378b5fe95397ff24d819f
Mike Lazzaro <lazzaro.mike@gmail.com>
2014-11-05 22:34:10 -0500

    Fixed missing zsh stuff

--- scripts/basic-packages.sh
+++ scripts/basic-packages.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+set -e
+
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root!"
     exit 1
 fi
 
-apt-get install -y git vim ctags zsh tree htop
+echo -e "\n\nSetting up basic environment"
+echo "============================"
 
+echo "Installing packages..."
+apt-get install -y git vim ctags zsh tree htop python python-dev python-setuptools > /dev/null
+
+echo "Installing pip..."
+easy_install pip > /dev/null
+
+echo "Installing python packages..."
+pip install virtualenvwrapper > /dev/null
+
+echo -e "Checking shell...\n"
 read -r -p "Do you want to set zsh as the default shell? [y/N] " response
 response=${response,,}    # tolower
 if [[ $response =~ ^(yes|y)$ ]]; then
 #    chsh $(which zsh)
-    usermod -s $(which zsh) mike
+    usermod -s $(which zsh) mike > /dev/null
 fi
-    
+
+
+
--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Clone (or update) the oh-my-zsh repo..."
 if [ -d ~/.oh-my-zsh ]; then
     git -C ~/.oh-my-zsh pull
 else
-    git clone --force git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh > /dev/null
 fi
 
-#cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+REPO_DIR=~/repos/powerline-fonts
+echo "Install the powerline fonts..."
+if [ -d $REPO_DIR ]; then
+    git -C $REPO_DIR pull
+else
+    git clone git@github.com:Lokaltog/powerline-fonts.git $REPO_DIR > /dev/null
+fi
 
+$REPO_DIR/install.sh > /dev/null
+
+
+echo "Get my standard .zshrc file..."
 cp $DIR/../dotfiles/.zshrc ~/.zshrc
